4dfcc4e2f9b90fcc45e90e04db3d09af24a25f93
anthonylol06 <anthonylocuhk@gmail.com>
2025-05-23 01:52:48 +0800

    feat: update config

--- svelte.config.js
+++ svelte.config.js
@@ -11,9 +11,18 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html',
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true
+		}),
 		alias: {
 			$component: 'src/component',
+		},
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
